49b0820ac09ee28a30bd91a1ff31d7fe46c28d16
Pablo Viana <via16091@uvg.edu.gt>
2021-03-03 22:29:39 -0600

    algoritmo sustitucion

--- algo_encriptacion.py
+++ algo_encriptacion.py
@@ -16,6 +16,7 @@ Referencias:
 
 -> https://www.tutorialspoint.com/cryptography_with_python/cryptography_with_python_decryption_of_transposition_cipher.htm
 -> https://www.tutorialspoint.com/cryptography_with_python/cryptography_with_python_encryption_of_transposition_cipher.htm
+-> programa one-time-pad desarrollado con anterioridad
 """
 
 import string
@@ -28,28 +29,38 @@ import math
 help = """ejecute los siguiente comandos para:
 Encriptar con transposicion: python algo_encriptacion.py -transE
 Desencriptar con transposicion: python algo_encriptacion.py -transD
+Encriptar con sustitución: python algo_encriptacion.py -sustE
+Encriptar con sustitución: python algo_encriptacion.py -sustD
 """
-
+# obtenemos los caracteres del abecedario en minuscula
+abecedario = string.ascii_lowercase
+sustitucion_abc = list(abecedario)
 
 def trans_encriptar(llave, mensaje):
     texto_encriptado = [''] * llave
-
+    #la llave marca el numero total de columnas de la tabla
     for col in range(llave):
+    	#primera posición siempre es el número de columna
         position = col
         while position < len(mensaje):
+        	#realizamos una tabla donde vamos escribiendo el mensaje de manera vertical, por así decirle
             texto_encriptado[col] += mensaje[position]
+            #Movemos la position una "llave" hacia adelante
             position += llave
     
     return ''.join(texto_encriptado)
 
 def trans_desencriptar(llave, mensaje):
+	#Calculamos el numero de columnas en base a longitud del mensaje y llave
 	numOfColumns = math.ceil(len(mensaje) / llave)
 	numOfRows = llave
+	#numero de celdas de la tabla que no se llenaron
 	numOfShadedBoxes = (numOfColumns * numOfRows) - len(mensaje)
 	plaintext = [''] * numOfColumns
 	col = 0
 	row = 0
 	
+	#Desencriptamos el mensaje reccoriendo la tabla
 	for symbol in mensaje:
 	   plaintext[col] += symbol
 	   col += 1
@@ -59,15 +70,47 @@ def trans_desencriptar(llave, mensaje):
 	
 	return ''.join(plaintext)
 
+def sust_encriptar(mensaje, llave):
+	texto_encriptado = ''
+	for index, char in enumerate(mensaje):
+		# buscamos el indice del primer caracter del mensaje en el abecedario
+		char_index = abecedario.index(char)
+		# Buscamos el indice del caracter encontrado en el arreglo llave[posicion indice en que va el ciclo]
+		llave_index = sustitucion_abc.index(llave[index])
+		# realizamos el cifrad
+		encriptado = (char_index + llave_index) % len(sustitucion_abc)
+		# armamos la palabra encriptad
+		texto_encriptado += abecedario[encriptado]
+
+	return texto_encriptado
+
+
+def sust_desencriptar(mensaje, llave):
+	# Revisa que los parametros sean strings validos para no computar un string vacio
+	if mensaje == '' or llave == '':
+		return ''
+
+	# Tomamos el indice del primer caracter del mensaje en el abecedario
+	char_index = abecedario.index(mensaje[0])
+	# Tomamos el indice del primer caracter de la llave en el abecedario
+	llave_index = sustitucion_abc.index(llave[0])
+
+	# Realizamos el Desencriptado
+	desencriptado = (char_index - llave_index) % len(sustitucion_abc)
+	# Buscamos un solo caracter desencriptado en el abecedario
+	character = abecedario[desencriptado]
+
+	# Recursivamente, buscamos los demas caracteres en el abecedario
+	return character + sust_desencriptar(mensaje[1:], llave[1:])
+
 if __name__ == '__main__':
-    availableOpt = ["-transE", "-transD"]
+    availableOpt = ["-transE", "-transD", "-sustE", "-sustD"]
     if len(sys.argv) == 1 or sys.argv[1] not in availableOpt:
         print(help)
         exit(0)
 
     mensaje = input("mensaje a descifrar: ")
 
-    
     if sys.argv[1] == availableOpt[0]:
         #Creamos llave de manera aleatoria
         llave_transposicion = random.randint(0,(len(mensaje)//2))
@@ -75,15 +118,46 @@ if __name__ == '__main__':
         text_file = open("llave_transposicion.txt", "w")
         text_file.write(str(llave_transposicion))
         text_file.close()
-
+        #Ecriptamos el texto con transposición
         texto_cifrado = trans_encriptar(llave_transposicion, mensaje)
-
+        #Imprimimos en pantalla
         print("texto cifrado:")
         print(texto_cifrado + '|')
+        #Copiamos mensaje encriptado al portapapeles
         pyperclip.copy(texto_cifrado)
     #Decifrar transposición
     elif sys.argv[1] == availableOpt[1]:
+    	#Abrimos archivo con la llave de transposición aleatoria
     	text_file = open("llave_transposicion.txt", "r")
     	myKey = text_file.read()
+    	#Desencriptamos el mensaje
     	plaintext = trans_desencriptar(int(myKey), mensaje)
+    	#Imprimos en pantalla y copiamos al portapapeles
+    	pyperclip.copy(plaintext)
     	print(plaintext)
+    #Encriptar sustitución
+    elif sys.argv[1] == availableOpt[2]:
+    	#Creamos la llave aleatoria que servira como criterio de sustitución para el mensaje
+    	dict_llave = list(string.ascii_lowercase)
+    	random.shuffle(dict_llave)
+    	llave = dict_llave[:len(mensaje)]
+    	llave_txt = ''.join([str(elem) for elem in llave])
+    	#La escribimos en archivo de texto
+    	text_file = open("llave_sustitucion.txt", "w")
+    	text_file.write(llave_txt)
+    	text_file.close()
+    	#Encriptamos con sustitución
+    	text_crip = sust_encriptar(mensaje.replace(" ",""), llave)
+    	#Imprimimos resultado en pantalla y copiamos al portapapeles
+    	pyperclip.copy(text_crip)
+    	print(text_crip)
+    #Desencriptar sustitucion
+    elif sys.argv[1] == availableOpt[3]:
+    	#Abrimos llave única y aleatoria de sustitución
+    	text_file = open("llave_sustitucion.txt", "r")
+    	llave = text_file.read()
+    	#Desciframos 
+    	text_crip = sust_desencriptar(mensaje, llave)
+    	#Imprimimos resultado en pantalla y copiamos al portapapeles
+    	pyperclip.copy(text_crip)
+    	print(text_crip)
